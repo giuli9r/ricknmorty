b597a48f97357668e047c3ffcf11cf273e32246c
giuli9r <giuli.rossetti9@gmail.com>
2024-05-13 21:23:07 -0300

    add styles to appdotvue

--- ricknmorty/src/App.vue
+++ ricknmorty/src/App.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="container">
     <h1>Rick and Morty</h1>
-    <ListCharacters>
-      
-    </ListCharacters>
+    <ListCharacters> </ListCharacters>
   </div>
   </template>
 
@@ -19,12 +17,33 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+:root {
+  --background-body: #24282F;
+  --background-card: #3C3E44;
+  --text-white: #FFFFFF;
+  --text-gray: #c0c0c0;
+  --text-orange: #FF9800;
+}
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  background-color: var(--background-body);
+  color: var(--text-white);
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+}
+h1 {
+  margin-bottom: 3rem;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.container {
+  width: 780px;
+  max-width: 80%;
+  margin: 5rem auto;
+}
+img {
+  width: 100%;
 }
 </style>
