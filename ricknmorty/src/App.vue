f8829a3f12af1d1d350e79753dc2778d397760f4
giuli9r <giuli.rossetti9@gmail.com>
2024-05-20 21:02:07 -0300

    funtionality FilterByName

--- ricknmorty/src/App.vue
+++ ricknmorty/src/App.vue
@@ -1,55 +1,58 @@
 <template>
-  <div class="container">
-    <h1>Rick and Morty</h1>
-    <FilterByStatus></FilterByStatus>
-    <FilterByGender></FilterByGender>
-    <ListCharacters> </ListCharacters>
-  </div>
-  </template>
+    <div class="container">
+        <h1>Rick and Morty</h1>
+        <FilterByStatus></FilterByStatus>
+        <FilterByGender></FilterByGender>
+        <FilterByName/>
+        <ListCharacters> </ListCharacters>
+    </div>
+</template>
 
 <script>
 import ListCharacters from './components/ListCharacters.vue'
 import FilterByStatus from './components/FilterByStatus.vue'
 import FilterByGender from './components/FilterByGender.vue'
+import FilterByName from './components/FilterByName.vue'
 
 export default {
-  name: 'App',
-  components: {
-    ListCharacters,
-    FilterByStatus,
-    FilterByGender
-  }
+    name: 'App',
+    components: {
+        ListCharacters,
+        FilterByStatus,
+        FilterByGender,
+        FilterByName
+    }
 }
 </script>
 
 <style lang="scss">
 :root {
-  --background-body: #24282F;
-  --background-card: #3C3E44;
-  --text-white: #FFFFFF;
-  --text-gray: #c0c0c0;
-  --text-orange: #FF9800;
+    --background-body: #24282F;
+    --background-card: #3C3E44;
+    --text-white: #FFFFFF;
+    --text-gray: #c0c0c0;
+    --text-orange: #FF9800;
 }
 * {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
 }
 body {
-  background-color: var(--background-body);
-  color: var(--text-white);
-  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    background-color: var(--background-body);
+    color: var(--text-white);
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 }
 h1 {
-  margin-bottom: 3rem;
-  text-align: center;
+    margin-bottom: 3rem;
+    text-align: center;
 }
 .container {
-  width: 780px;
-  max-width: 80%;
-  margin: 5rem auto;
+    width: 780px;
+    max-width: 80%;
+    margin: 5rem auto;
 }
 img {
-  width: 100%;
+    width: 100%;
 }
 </style>
--- ricknmorty/src/store/index.js
+++ ricknmorty/src/store/index.js
@@ -2,52 +2,62 @@ import { createStore } from 'vuex'
 
 // new store instance.
 export default createStore({
-  state: {
-    characters: [],
-    charactersFilter: [],
-    pagination: []
-  },
-  getters: {
-  },
-  // modifica los diferentes valores de state{} mediante actions
-  mutations: {
-    setCharacter(state, payload) {
-      state.characters = payload
+    state: {
+        characters: [],
+        charactersFilter: [],
+        pagination: []
     },
-    setCharacterFilter(state, payload){
-      state.charactersFilter = payload
+    getters: {
     },
-    setPaging(state, payload){
-      state.pagination = payload
+    // modifica los diferentes valores de state{} mediante actions
+    mutations: {
+        setCharacter(state, payload) {
+            state.characters = payload
+        },
+        setCharacterFilter(state, payload){
+            state.charactersFilter = payload
+        },
+        setPaging(state, payload){
+            state.pagination = payload
+        }
+    },
+    actions: {
+        async getCharacters({commit}){ 
+            try {
+                const response = await fetch ('https://rickandmortyapi.com/api/character')
+                const data = await response.json()
+                commit('setCharacter', data.results)
+                commit('setCharacterFilter', data.results)
+                commit('setPaging', data.info)
+                console.log(data)
+            } catch (error) {
+                console.log(error)
+                alert('Error!')
+            }
+        },
+        filterByStatus({commit, state}, status) {
+            const filter = state.characters.filter((character) => {
+                return character.status.includes(status)
+            })
+            commit('setCharacterFilter', filter)
+        },
+        filterByGender({commit, state}, gender){
+            const filterGender = state.characters.filter((character) => {
+                return character.gender.includes(gender)
+            })
+            commit('setCharacterFilter', filterGender)
+        },
+        filterByName({commit, state}, name) {
+            const formatName = name.toLowerCase();
+            const results = state.characters.filter((character) => {
+                let characterName = character.name.toLowerCase();
+                if (characterName.includes(formatName)){
+                    return character
+                }
+            })
+            commit('setCharacterFilter', results)
+        }
+    },
+    modules: {
     }
-  },
-  actions: {
-    async getCharacters({commit}){ 
-      try {
-        const response = await fetch ('https://rickandmortyapi.com/api/character')
-        const data = await response.json()
-        commit('setCharacter', data.results)
-        commit('setCharacterFilter', data.results)
-        commit('setPaging', data.info)
-        console.log(data)
-      } catch (error) {
-        console.log(error)
-        alert('Error!')
-      }
-    },
-    filterByStatus({commit, state}, status) {
-      const filter = state.characters.filter((character) => {
-        return character.status.includes(status)
-      })
-      commit('setCharacterFilter', filter)
-    },
-    filterByGender({commit, state}, gender){
-      const filterGender = state.characters.filter((character) => {
-        return character.gender.includes(gender)
-      })
-      commit('setCharacterFilter', filterGender)
-    }
-  },
-  modules: {
-  }
 })
